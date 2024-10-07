d006111ef3d61243d0e2e87bb6056d026481f5be
Therry Walker <therrywalker@gmail.com>
2024-10-07 08:04:25 -0400

    conflict resolved

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,13 @@ Deploy in the dev environment
 
 deployment5 master
 deployment4
+<<<<<<< HEAD
 deploy to staging env
 echo good job.
 Tested and accepted
 
 deployment4
 
+=======
+deployment5 master
+>>>>>>> 973fe1f (Error modified)
