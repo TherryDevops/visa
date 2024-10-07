682bcaaf8b01d52b34e8dc16bea35e1b8ca89da7
Therry Walker <therrywalker@gmail.com>
2024-10-07 08:03:14 -0400

    conflict resolved

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 Deploy java application
 Deploy python apps
 Deploy in the dev environment
+
 deployment5 master
 deployment4
 deploy to staging env
 echo good job.
 Tested and accepted
+
+deployment4
+
